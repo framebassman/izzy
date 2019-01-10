9128c5a820f37948aada4373c1403f7517c8a8c7
Dmitry Romashov <framebassman@gmail.com>
2019-01-10 11:40:17 +0300

    add broken test

--- Izzy.Web/Client/src/__tests__/Calculator.spec.ts
+++ Izzy.Web/Client/src/__tests__/Calculator.spec.ts
@@ -1,6 +1,8 @@
 import React from 'react';
 import {mount} from 'enzyme';
+import App from '../App';
 
 it('should add text field for person', done => {
+  const component = mount(App);
   done();
 });
