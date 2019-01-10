85039f17ba4161cf7b36a02751809856090faccf
Dmitry Romashov <framebassman@gmail.com>
2019-01-11 01:11:34 +0300

    add first real enzyme test

--- Izzy.Web/Client/src/__tests__/App.spec.tsx
+++ Izzy.Web/Client/src/__tests__/App.spec.tsx
@@ -1,17 +1,35 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount, ReactWrapper } from 'enzyme';
 
 import App from '../App';
+import { Provider } from 'react-redux';
+
+import configureStore from '../store/configureStore';
+const initialState = (window as any).initialReduxState;
+const store = configureStore(initialState);
 
 describe('<App />', () => {
-  it ('Updates heading on setState', done => {
-    const el = shallow(<App />);
+  let app: ReactWrapper;
 
-    expect(el.find('h3').text()).toBe('');
+  beforeEach(() => {
+    app = mount(
+      <Provider store={store}>
+        <App/>
+      </Provider>
+    );
+  });
 
-    el.setState({ result: 'A' }, () => {
-        expect(el.update().find('h3.result').text()).toBe('A');
-        done();
-    });
+  it ('Add person after click on Add button', done => {
+    // Arrange
+    const personsBefore = app.find('.person').length;
+
+    // Act
+    const addButton = app.find('#add_button').hostNodes();
+    addButton.simulate('click');
+
+    // Assert
+    const personsAfter = app.find('.person').length;
+    expect(personsAfter - personsBefore).toEqual(1);
+    done();
   });
 });
