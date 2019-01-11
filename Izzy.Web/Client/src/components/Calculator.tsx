0fcc609c403206c9dde837a8eb3ac0d7c4328558
Dmitry Romashov <framebassman@gmail.com>
2019-01-11 11:43:24 +0300

    change form to FormControl from material

--- Izzy.Web/Client/src/components/Calculator.tsx
+++ Izzy.Web/Client/src/components/Calculator.tsx
@@ -3,6 +3,7 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { actionCreators } from '../store/Calculator';
 import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
 import { Persons } from './Persons';
 import './Calculator.css';
 
@@ -11,11 +12,11 @@ class Calculator extends Component<any, any> {
     const {increment, quantity} = this.props;
     return (
       <div className="calculator">
-        <form>
+        <FormControl>
           <h1>Замиокулькас</h1>
           <Button id="add" onClick={increment}>Добавить</Button>
           <Persons quantity={quantity}/>
-        </form>
+        </FormControl>
         <Button color="primary">Рассчитать</Button>
       </div>
     );
