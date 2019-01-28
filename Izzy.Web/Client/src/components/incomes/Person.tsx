52e98b2f7aff18f39a9cecf54108ddf44be489a0
Dmitry Romashov <framebassman@gmail.com>
2019-01-28 11:34:43 +0300

    only numbers in roubles field

--- Izzy.Web/Client/src/components/incomes/Person.tsx
+++ Izzy.Web/Client/src/components/incomes/Person.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Input from '@material-ui/core/Input';
+import InputAdornment from '@material-ui/core/InputAdornment';
 import './Person.css';
 
 export class Person extends Component<any, any> {
@@ -7,8 +8,15 @@ export class Person extends Component<any, any> {
     const {man} = this.props;
     return (
       <div className="person">
-        <Input className="person__input" placeholder={man.name}/>
-        <Input className="person__input" placeholder="рублей"/>
+        <Input
+          className="person__input"
+          placeholder={man.name} 
+        />
+        <Input 
+          className="person__input"
+          placeholder="рублей"
+          type="number"
+        />
       </div>
     )
   }
