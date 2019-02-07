690351e0fb474d6faf3715abd11917ff3eab33c5
Dmitry Romashov <framebassman@gmail.com>
2019-02-07 21:58:32 +0300

    catch corner case

--- Izzy.Web/Client/src/components/outcomes/Transfers.tsx
+++ Izzy.Web/Client/src/components/outcomes/Transfers.tsx
@@ -3,15 +3,26 @@ import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 const petrovich = require('petrovich');
 
-export class Transfers extends Component<any, any> {
-  destInPropriateCase(dest: string): string {
-    const first = dest.trim().split(' ')[0];
-    const person = { gender: 'male', first };
-    const result = petrovich(person, 'dative').first;
-    const withoutFirst = dest.replace(first, '');
-    return result + withoutFirst;
-  }
+function destInPropriateCase(dest: string): string {
+  const first = dest.trim().split(' ')[0];
+  const result = conjugateToDativeCase(first);
+  const withoutFirst = dest.replace(first, '');
+  return result + withoutFirst;
+}
 
+function conjugateToDativeCase(origin: string): string {
+  if (origin === 'Я') {
+    return 'Мне';
+  }
+  if (origin === 'я') {
+    return 'мне';
+  }
+  const person = { gender: 'male', origin };
+  const result = petrovich(person, 'dative').first;
+  return result;
+}
+
+export class Transfers extends Component<any, any> {
   render() {
     const { src } = this.props;
     let inputs = [];
@@ -20,7 +31,7 @@ export class Transfers extends Component<any, any> {
       inputs.push(
         <TableRow key={i++}>
           <TableCell>{tr.from}</TableCell>
-          <TableCell>{this.destInPropriateCase(tr.to)}</TableCell>
+          <TableCell>{destInPropriateCase(tr.to)}</TableCell>
           <TableCell>{tr.roubles}</TableCell>
         </TableRow>
       )
