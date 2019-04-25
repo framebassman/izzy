e04306e3aa909a4db38cf1f72789d1ed9f38ceaf
Dmitry Romashov <framebassman@gmail.com>
2019-04-25 16:08:44 +0300

    Feature/ci (#18)
    * test ci
    
    * try to release on github
    
    * deploy only in master

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,5 +1,5 @@
 if [[ $TRAVIS_BUILD_NUMBER != "" ]]; then
-  docker build --file ./Dockerfile . --tag framebassman/izzy:1.0.$TRAVIS_BUILD_NUMBER
+  docker build --file ./Dockerfile . --tag framebassman/izzy:1.1.$TRAVIS_BUILD_NUMBER
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
   docker push framebassman/izzy
 fi
