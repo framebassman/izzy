cc32a1247b68cfcff073da17f9b209a321f609aa
Dmitry Romashov <framebassman@gmail.com>
2019-05-28 21:19:32 +0300

    Bug/23 (#25)
    * Feature/ci (#18)
    
    * test ci
    
    * try to release on github
    
    * deploy only in master
    
    * beautify readme
    
    * try to fix deploy in master only
    
    * Feature/ci (#20)
    
    * test
    
    * test
    
    * fix
    
    * fix
    
    * dont deploy in pr
    
    * fix base url
    
    * fix warnings
    
    * update packages
    
    * #23 add tests
    
    * add matcher
    
    * ignore rider files
    
    * add more tests
    
    * fix one case, break another
    
    * fix #23

--- Izzy.Web/Model/Receipt.cs
+++ Izzy.Web/Model/Receipt.cs
@@ -17,22 +17,49 @@ namespace Izzy.Web.Model
         {
             var transfers = new List<Transfer>();
             var total = this._persons.Sum(p => p.Roubles);
-            var roublesPerOne = Decimal.Round(total / this._persons.Count(), 2);
-            var personsTop = this._persons
-                .OrderByDescending(p => p.Roubles);
-            var spender = personsTop.First();
-            var personsExceptSpender = personsTop.Skip(1);
-            foreach (var person in personsExceptSpender)
+            var middle = Decimal.Round(total / this._persons.Count(), 2);
+            var personsTop = this._persons.OrderByDescending(p => p.Roubles).ToList();
+            var moreThanMiddle = personsTop.Where(p => p.Roubles >= middle).ToList();
+            var lessThanMiddle = personsTop.Except(moreThanMiddle).ToList();
+            for (int i = 0; i < moreThanMiddle.Count; i++)
             {
-                transfers.Add(
-                    new DifferenceTransfer(
-                        person,
-                        spender,
-                        roublesPerOne - person.Roubles
-                    )
-                );
+                var spender = moreThanMiddle[i];
+                for (int j = lessThanMiddle.Count - 1; j >= 0; j--)
+                {
+                    if (spender.Roubles - middle != 0)
+                    {
+                        var debtor = lessThanMiddle[j];
+                        var debtorDiff = middle - debtor.Roubles;
+                        var spenderDiff = spender.Roubles - middle;
+                        if (debtorDiff > spenderDiff)
+                        {
+                            transfers.Add(new Transfer(debtor, spender, spenderDiff));
+                            debtor.Roubles += spenderDiff;
+                            spender.Roubles -= spenderDiff;
+                        }
+                        else
+                        {
+                            transfers.Add(new Transfer(debtor, spender, debtorDiff));
+                            debtor.Roubles += debtorDiff;
+                            spender.Roubles -= debtorDiff;
+                        }
+                    }
+                }
             }
+
             return transfers;
+        }
+
+        private Decimal diff(Person debtor, Person spender, Decimal middle)
+        {
+            if (debtor.Roubles > spender.Roubles - middle)
+            {
+                return spender.Roubles - middle;
+            }
+            else
+            {
+                return debtor.Roubles;
+            }
         }
     }
 }
