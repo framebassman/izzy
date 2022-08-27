feae5640e3c3e89cd85e858647921af0bef9d22d
Dmitry Romashov <framebassman@gmail.com>
2022-08-27 11:45:01 +0200

    Fix

--- Izzy.Web/Client/src/components/incomes/Incomes.tsx
+++ Izzy.Web/Client/src/components/incomes/Incomes.tsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import FormControl from '@material-ui/core/FormControl';
 import TextField from '@material-ui/core/TextField';
-import Input from '@material-ui/core/Input';
 import { Formik, Form, Field, FieldArray, FieldProps } from 'formik';
 import './Person.css';
 import { RandomPerson } from '../../store/model/RandomPerson';
